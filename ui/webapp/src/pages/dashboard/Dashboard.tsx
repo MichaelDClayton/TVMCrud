6a6f92b8055c32ec13426386c59764fb759e9927
Michael D Clayton <michael.d.clayton@hotmail.com>
2024-10-27 05:46:33 -0400

    users are successfully showing in the browser as well.

--- ui/webapp/src/pages/dashboard/Dashboard.tsx
+++ ui/webapp/src/pages/dashboard/Dashboard.tsx
@@ -1,26 +1,24 @@
 import UserList from "../../components/UserList";
 import { User} from "../../model/User";
 import apiClient from "../../config/apiClient";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 const Dashboard = () =>{
+const [users, setUsers] = useState<User[]>([]);
+const [error, setErrors] = useState(null);
+const [isLoading, setLoader] = useState(false);
 useEffect(() => {
     //call backend api.
-    apiClient.get("http://localhost:8080/api/users")
-    .then((response) => console.log(response))
-    .catch((error) => console.log(error));
+    apiClient
+    .get("http://localhost:8080/api/users")
+    .then((response) => {
+        setUsers(response.data);
+        setLoader(false);
+        })
+    .catch((error) => setErrors(error))
+    .finally(() => setLoader(false));
     }, []);
 
 
-     const users: User[] = [
-                    {id:1,
-                        firstName: "Mike",
-                        lastName: "Clayton",
-                        email:"mc@aol.com"},
-                    {id:2,
-                        firstName: "John",
-                        lastName: "Smith",
-                        email:"js@fish.net"}
-                    ];
 
 
 return <div>

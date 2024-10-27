3005acaa895f78af57d1502f2545221b4933b830
Michael D Clayton <michael.d.clayton@hotmail.com>
2024-10-27 18:21:52 -0400

    added custom hook useUsers.ts.

--- ui/webapp/src/pages/dashboard/Dashboard.tsx
+++ ui/webapp/src/pages/dashboard/Dashboard.tsx
@@ -1,34 +1,15 @@
 import UserList from "../../components/UserList";
-import { User} from "../../model/User";
-import apiClient from "../../config/apiClient";
-import { useEffect, useState } from "react";
-import getUsers from "../../services/user-service";
+import useUsers from "../../hooks/useUsers";
 
 const Dashboard = () =>{
-const [users, setUsers] = useState<User[]>([]);
-const [error, setErrors] = useState(null);
-const [isLoading, setLoader] = useState(false);
-useEffect(() => {
-    setLoader(true);
-    //call backend api.
-    //apiClient
-    //.get("http://localhost:8080/api/users")
-     getUsers()
-    .then((response) => {
-        setUsers(response.data);
-        setLoader(false);
-        })
-    .catch((error) => setErrors(error))
-    .finally(() => setLoader(false));
-    }, []);
 
+const {users, error, isLoading} = useUsers();
 
-
-
-return <div>
+return (<div>
 {isLoading && <p>Loading...</p>}
 {error && <p>{error}</p>}
 <UserList users={users}/>
 </div>
+);
     };
 export default Dashboard

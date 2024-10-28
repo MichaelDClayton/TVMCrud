bd60817d9aa4e67fe7d1781879cc7830cc1cca9f
Michael D Clayton <michael.d.clayton@hotmail.com>
2024-10-28 16:43:01 -0400

    confirm dialog for delete working.

--- ui/webapp/src/pages/user/UserDetails.tsx
+++ ui/webapp/src/pages/user/UserDetails.tsx
@@ -3,9 +3,24 @@ import { useParams } from "react-router-dom";
 import { getUserById } from "../../services/user-service";
 import { User } from "../../model/User";
 import useUserById from "../../hooks/useUserById"
+import ConfirmDialog from "../../components/ConfirmDialog";
+
 const UserDetails = () =>{
+
     const { id } = useParams<{id: string}>();
     const {user, errors, isLoading} = useUserById(id!);
+    const [showDialog, setShowDialog] = useState<boolean>(false);
+    if(!id){
+            return <p className="text-danger">Invalid User ID</p>
+            }
+    const handleConfirm = () => {
+        console.log("Confirm is clicked");
+        setShowDialog(false);
+        }
+    const handleCancel = () => {
+            console.log("Cancel is clicked");
+            setShowDialog(false);
+            }
 
 
     return (
@@ -13,7 +28,7 @@ const UserDetails = () =>{
             {isLoading && <p>Loading...</p>}
             {errors && <p className="text-danger">errors</p>}
                 <div className="d-flex flex-row-reverse mb-2">
-                    <button className="btn btn-sm btn-danger">Delete</button>
+                    <button className="btn btn-sm btn-danger" onClick={() => setShowDialog(true)}>Delete</button>
                     <button className="btn btn-sm btn-warning mx-2">Edit</button>
                     <button className="btn btn-sm btn-secondary">Back</button>
                 </div>
@@ -35,6 +50,12 @@ const UserDetails = () =>{
                             </table>
                         </div>
                     </div>
+                    <ConfirmDialog title="Confirm Delete"
+                    message="Are you sure you want to delete this User?"
+                    show={showDialog}
+                    onCancel={handleCancel}
+                    onConfirm={handleConfirm}
+                    />
         </div>
         )
     }
